53fed93a9a6bf565f0216690a62666d36907a8d0
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2021-07-31 15:00:59 +0300

    Correct adding and matching functions

--- main.cpp
+++ main.cpp
@@ -4,65 +4,99 @@
 using namespace std;
 typedef char* String;
 
-void initialiseList(){
+int getListSize(char** list){
+    char** current = list;
+    int size = 0;
+    while (current!= nullptr){
+        size++;
+        current = &current[1];
+    }
+    return size;
+}
+
+void addString(char**& list,String t){
+    char** new_node = (String*) malloc(2);
+    new_node[0] = t;
+    new_node[1] = nullptr;
+    if(list== nullptr){
+        list = new_node;
+        return;
+    }
+    char** current = list;
+    while (current[1]!= nullptr){
+        current = reinterpret_cast<char **>(current[1]);
+    }
+    current[1] = reinterpret_cast<char *>(new_node);
+}
+
+void printList(char** list){
+    char** current = list;
+    while (current!= nullptr){
+        cout<<current[0]<<endl;
+        current = reinterpret_cast<char **>(current[1]);
+    }
+}
+
+void deleteString(char** list,String t){
 
 }
 
-int getListSize(){
-
-}
-
-void addString(String t){
-
-}
-
-void deleteString(String t){
-
-}
-
-int indexOfExactMatch(String t){
-
+int indexOfExactMatch(char** list,String t){
+    char** current = list;
+    int current_position = 0;
+    bool found = false;
+    while (current!= nullptr){
+        if(*current[0]==*t) { found=true;break; }
+        current = reinterpret_cast<char **>(current[1]);
+        current_position++;
+    }
+    if(found)return current_position;
+    return -1;
 }
 
 int main() {
     int command;
     int n;
     String t;
+    char** list = nullptr;
     do{
         cout<<"Enter 1 to add string\n";
         cout<<"Enter 2 to remove string\n";
         cout<<"Enter 3 to get number of items in the list\n";
         cout<<"Enter 4 to get the index position of the first exact match of specified string\n";
+        cout<<"Enter 5 to print list\n";
         cout<<"Enter 0 to exit\n";
         cin>>command;
         switch (command) {
             case 1:
-                cout<<"Enter lenght of string to add\n";
+                cout<<"Enter length of string to add\n";
                 cin>>n;
                 t = (String) malloc(n + 1);
-                cout<<"Enter string\n";
-                gets(t);
-                addString(t);
+                cout<<"Enter string: ";
+                cin>>t;
+                addString(list,t);
                 break;
             case 2:
-                cout<<"Enter string to remove\n";
+                cout<<"Enter length of string to remove\n";
                 cin>>n;
                 t = (String) malloc(n + 1);
                 cout<<"Enter string\n";
-                gets(t);
-                deleteString(t);
+                cin>>t;
+                deleteString(list,t);
                 break;
             case 3:
-                cout<<"Size of list: "<<getListSize()<<endl;
+                cout<<"Size of list: "<<getListSize(list)<<endl;
                 break;
             case 4:
-                cout<<"Enter string to find first exact match\n";
-                cout<<"Enter string to remove\n";
+                cout<<"Enter size of string\n";
                 cin>>n;
                 t = (String) malloc(n + 1);
                 cout<<"Enter string\n";
-                gets(t);
-                indexOfExactMatch(t);
+                cin>>t;
+                cout<<indexOfExactMatch(list,t)<<endl;
+                break;
+            case 5:
+                printList(list);
                 break;
             default:
                 cout<<"Command not recognized\n";
