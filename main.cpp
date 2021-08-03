3e32c6fbbb5c54576e7fa8e3a055c39bf8e0e333
PaulMoroz <55598887+PaulMoroz@users.noreply.github.com>
2021-08-03 17:49:48 +0300

    Remade interface, added initialisation of list

--- main.cpp
+++ main.cpp
@@ -4,8 +4,14 @@
 using namespace std;
 typedef char* String;
 
+void StringListInit(char*** list){
+    (*list) = (char** ) malloc(2);
+    (*list)[0] = "123";
+    (*list)[1] = nullptr;
+}
+
 int getListSize(char** list){
-    char** current = list;
+    char** current = reinterpret_cast<char **>(list[1]);
     int size = 0;
     while (current!= nullptr){
         size++;
@@ -14,17 +20,32 @@ int getListSize(char** list){
     return size;
 }
 
-char** getLastElement(char ** list){
+void StringListRemoveDuplicates(char** list,String t){
+    /*char** new_node = (String*) malloc(2);
+    new_node[0] = t;
+    new_node[1] = nullptr;
+    if(list== nullptr){
+        list = new_node;
+        return;
+    }
     char** current = list;
-    while (current!= nullptr){
+    while (current[1]!= nullptr){
         current = reinterpret_cast<char **>(current[1]);
     }
-    return current;
+    current[1] = reinterpret_cast<char *>(new_node);*/
 }
 
-void addString(char**& list,String t){
+void printList(char** list){
+    char** current = reinterpret_cast<char **>(list[1]);
+    while (current!= nullptr){
+        cout<<current[0]<<endl;
+        current = reinterpret_cast<char **>(current[1]);
+    }
+}
+
+void StringListAdd(char** list,String str){
     char** new_node = (String*) malloc(2);
-    new_node[0] = t;
+    new_node[0] = str;
     new_node[1] = nullptr;
     if(list== nullptr){
         list = new_node;
@@ -37,20 +58,12 @@ void addString(char**& list,String t){
     current[1] = reinterpret_cast<char *>(new_node);
 }
 
-void printList(char** list){
-    char** current = list;
-    while (current!= nullptr){
-        cout<<current[0]<<endl;
-        current = reinterpret_cast<char **>(current[1]);
-    }
-}
-
-int indexOfExactMatch(char** list,String t){
-    char** current = list;
+int StringListIndexOf(char** list,const String str){
+    char** current = reinterpret_cast<char **>(list[1]);
     int current_position = 0;
     bool found = false;
     while (current!= nullptr){
-        if(*current[0]==*t) { found=true;break; }
+        if(*current[0]==*str) { found=true;break; }
         current = reinterpret_cast<char **>(current[1]);
         current_position++;
     }
@@ -58,41 +71,35 @@ int indexOfExactMatch(char** list,String t){
     return -1;
 }
 
-void deleteString(char**& list, int pos){
-    if(pos >= getListSize(list))return;
-    if(pos == 0){
-        char** temp = list;
-        list = reinterpret_cast<char **>(list[1]);
-        free(temp);
-        return;
-    }
-    int curr_pos = 0;
+//!FIX free error
+void StringListRemove(char** list, String str){
     char** current = list;
-    while (curr_pos+1!=pos){
-        current = reinterpret_cast<char **>(current[1]);
-        curr_pos++;
+    while (current[1]!= nullptr){
+        char** temp = reinterpret_cast<char **>(current[1]);
+        if(temp!=nullptr  && *(temp)[0]==*str){
+            current[1] = temp[1];
+            free(temp);
+        }else{
+            current = reinterpret_cast<char **>(current[1]);
+        }
     }
-    char** temp = reinterpret_cast<char **>(current[1]);
-    current[1] = temp[1];
-    free(temp);
 }
 
-void replaceString(char**& list,int pos, String new_string){
-    if(pos >= getListSize(list))return;
-    char** current = list;
-    int curr_pos = 0;
-    while (curr_pos!=pos){
+void StringListReplaceInStrings(char** list, char* before, char* after){
+    char** current = reinterpret_cast<char **>(list[1]);
+    while (current!= nullptr){
+        if(*current[0]==*before){
+            char* t = current[0];
+            current[0] = after;
+            free(t);
+        }
         current = reinterpret_cast<char **>(current[1]);
-        curr_pos++;
     }
-    char* temp = current[0];
-    free(temp);
-    current[0] = new_string;
 }
 
-void sortList(char**& list){
+void StringListSort(char** list){
     int n = getListSize(list);
-    char** left = list;
+    char**  left = reinterpret_cast<char **>(list[1]);
     char** right;
     for(int i=0;i<n;i++){
         right = reinterpret_cast<char **>(left[1]);
@@ -108,20 +115,32 @@ void sortList(char**& list){
     }
 }
 
-void removeDuplicates(char** list){
+void StringListRemoveDuplicates(char** list){
+    /*
     char** left = list;
     char** right = list;
     while (left[1]!= nullptr){
-
-    }
+        if(right!=nullptr){
+            return; = reinterpret_cast<char **>(left[1]);
+            if(reinterpret_cast<char *>(right[1][0]) == left[0]){
+                char** temp = reinterpret_cast<char **>(right[1]);
+                right[1] = temp[1];
+                free(temp);
+            }
+            if(left[1]!= nullptr)right = reinterpret_cast<char **>(left[1]);
+        }
+    }*/
 }
 
 int main() {
     int command;
     int n;
     String t;
+    String oldStr;
+    String newStr;
     char** list = nullptr;
     int pos;
+    StringListInit(&list);
     do{
         cout<<"Enter 1 to add string\n";
         cout<<"Enter 2 to remove string\n";
@@ -130,6 +149,7 @@ int main() {
         cout<<"Enter 5 to print list\n";
         cout<<"Enter 6 to replace string in list\n";
         cout<<"Enter 7 to sort list\n";
+        cout<<"Enter 8 to remove duplicates from list\n";
         cout<<"Enter 0 to exit\n";
         cin>>command;
         switch (command) {
@@ -139,12 +159,15 @@ int main() {
                 t = (String) malloc(n + 1);
                 cout<<"Enter string: ";
                 cin>>t;
-                addString(list,t);
+                StringListAdd(list,t);
                 break;
             case 2:
-                cout<<"Enter index of string to remove(numbers from 0)\n";
-                cin>>pos;
-                deleteString(list,pos);
+                cout<<"Enter length of string to delete\n";
+                cin>>n;
+                t = (String) malloc(n + 1);
+                cout<<"Enter string: ";
+                cin>>t;
+                StringListRemove(list,t);
                 break;
             case 3:
                 cout<<"Size of list: "<<getListSize(list)<<endl;
@@ -155,23 +178,29 @@ int main() {
                 t = (String) malloc(n + 1);
                 cout<<"Enter string\n";
                 cin>>t;
-                cout<<indexOfExactMatch(list,t)<<endl;
+                cout<<StringListIndexOf(list,t)<<endl;
                 break;
             case 5:
                 printList(list);
                 break;
             case 6:
-                cout<<"Enter index of string to replace(numbers from 0)\n";
-                cin>>pos;
+                cout<<"Enter length of old string\n";
+                cin>>n;
+                oldStr = (String) malloc(n + 1);
+                cout<<"Enter string: ";
+                cin>>oldStr;
                 cout<<"Enter length of new string\n";
                 cin>>n;
-                t = (String) malloc(n + 1);
+                newStr = (String) malloc(n + 1);
                 cout<<"Enter string: ";
-                cin>>t;
-                replaceString(list,pos,t);
+                cin>>newStr;
+                StringListReplaceInStrings(list,oldStr,newStr);
                 break;
             case 7:
-                sortList(list);
+                StringListSort(list);
+                break;
+            case 8:
+                StringListRemoveDuplicates(list);
                 break;
             default:
                 cout<<"Command not recognized\n";
